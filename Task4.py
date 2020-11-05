e51f676f2e2af471b973840acfc598eafc144526
RoseMagura <magurarm@guilford.edu>
2020-11-05 12:03:53 -0500

    Feat: Finish Task4

--- Task4.py
+++ Task4.py
@@ -25,3 +25,26 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+callers = {}
+for call in calls:
+    if call[0] not in callers:
+        callers[call[0]] = [1, 0, 0, 0]
+    else:
+        callers[call[0]][0] += 1
+    if call[1] in callers:
+        callers[call[1]][1] += 1
+
+for text in texts:
+    if text[0] in callers:
+        callers[text[0]][2] += 1
+    if text[1] in callers:
+        callers[text[1]][3] += 1
+
+suspicious = []
+for caller in callers:
+    if callers[caller][1] == 0 and callers[caller][2] == 0 and callers[caller][3] == 0:
+        suspicious.append(caller)
+
+print("These numbers could be telemarketers: ")
+for s in sorted(suspicious):
+    print(s)
